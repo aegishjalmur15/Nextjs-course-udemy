ef579a75e4492822685f76a692d3ccada5a6a3d6
luan AC <luanalbucan@hotmail.com>
2022-05-03 16:20:43 -0300

    add pasta icons

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,6 +1,7 @@
 import Button from "../ui/button";
-
-
+import DateIcon from '../../icons/date-icon';
+import AddressIcon from '../../icons/address-icon';
+import ArrowRightIcon from '../../icons/arrow-right-icon';
 import styles from './event-item.module.css';
 
 function EventItem(props)
@@ -24,15 +25,20 @@ function EventItem(props)
             <div className={styles.summary}>
             <h2>{title}</h2>
             <div className={styles.date}>
+                <DateIcon />
                 <time>{humanReadableDate}</time>
             </div>
             <div className={styles.address}>
+                <AddressIcon />
                 <address>{formattedAddress}</address>
             </div>
             </div>
         </div>
         <div className={styles.actions}>
-            <Button link={exploreLink}>Explore Event</Button>
+            <Button link={exploreLink}>                
+                <span>Explore Event</span>
+                <span className={styles.icon}><ArrowRightIcon /></span>
+                </Button>
         </div>
     </li>)
 }
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,6 +1,23 @@
+import {useRouter} from 'next/router';
+import {getEventById} from '../../data/dummy-data';
+
 function EventsDetailPage()
 {
-    return <div>event detail</div>
+    const router = useRouter();
+    const eventId = router.query.eventId;
+
+    const event = getEventById(eventId);
+
+    if(!event){
+        return <p>No Event Found!</p>
+    }
+
+
+    return (
+    <div>
+        <h1>Event Detail</h1>
+    </div>
+    );
 }
 
 export default EventsDetailPage;
